db2f3467f3627ad77f3cef6b4ccdefd036992392
pulperi <pulperi@users.noreply.github.com>
2015-05-05 17:35:39 +0300

    teht 53

--- public/app/controllers/show_message_controller.js
+++ public/app/controllers/show_message_controller.js
@@ -1,3 +1,16 @@
 FoorumApp.controller('ShowMessageController', function($scope, $routeParams, Api){
   // Toteuta kontrolleri tähän
+    Api.getMessage($routeParams.id).success(function(message) {
+        $scope.message = message;
+    });
+    
+    $scope.newReply = { content: "" };
+    
+    $scope.addReply = function() {
+        if ($scope.newReply.content !== "")
+            Api.addReply($scope.newReply, $routeParams.id).success(function(reply) {
+                $scope.message.Replies.push(reply);
+            });
+    };
+    
 });
--- public/app/controllers/show_topic_controller.js
+++ public/app/controllers/show_topic_controller.js
@@ -4,4 +4,13 @@ FoorumApp.controller('ShowTopicController', function($scope, $routeParams, $loca
     }).error(function() {
         $location.path('/');
     });
+    
+    $scope.newMessage = { title: "", content: ""};
+    
+    $scope.addMessage = function() {
+        if ($scope.newMessage.title !== "" && $scope.newMessage.content !== "")
+            Api.addMessage($scope.newMessage, $routeParams.id).success(function (message) {
+                $location.path('/messages/' + message.id); 
+            });
+    };
 });
