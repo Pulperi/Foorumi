243669183eb7f069d2993b42a62a9bcd049a3c7f
pulperi <pulperi@users.noreply.github.com>
2015-05-05 20:27:51 +0300

    Sovellus Herokuun

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,16 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('foorumi', '', '', {
-  dialect: 'sqlite',
-  storage: 'db/database.sqlite'
+//var sequelize = new Sequelize('postgres://debqoredfowagh:LlQNFxD3CnQ94i4xBfTf-0gCB_@ec2-23-21-96-129.compute-1.amazonaws.com:5432/d96132ehfmqqj6', '', '', {
+//  dialect: 'sqlite',
+//  storage: 'db/database.sqlite'
+//});
+
+var sequelize = new Sequelize('postgres://debqoredfowagh:LlQNFxD3CnQ94i4xBfTf-0gCB_@ec2-23-21-96-129.compute-1.amazonaws.com:5432/d96132ehfmqqj6', {
+    dialect: 'postgres',
+    protocol: 'postgres'
 });
 
 module.exports = {
-  DataTypes: Sequelize,
-  sequelize: sequelize
+    DataTypes: Sequelize,
+    sequelize: sequelize
 };
